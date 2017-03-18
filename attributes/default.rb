29d436384c129a8e068850d2af948665d9643dbd
Brian Dwyer <bdwyer@ieee.org>
2017-03-18 18:24:37 -0400

    Fix RHEL
    String comparisons can be problematic

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,10 +11,10 @@ default['rabbitmq']['deb_package_url'] = "https://www.rabbitmq.com/releases/rabb
 
 case node['platform_family']
 when 'rhel', 'fedora'
-  case node['platform_version']
-  when <= '6'
+  case node['platform_version'].to_i
+  when <= 6
     default['rabbitmq']['rpm_package'] = "rabbitmq-server-#{node['rabbitmq']['version']}-1.el6.noarch.rpm"
-  when > '6'
+  when > 6
     default['rabbitmq']['rpm_package'] = "rabbitmq-server-#{node['rabbitmq']['version']}-1.el7.noarch.rpm"
   end
 when 'suse'
